45f8291bd24784d1a3765361c2908deac9ec4e9c
keshavg1407 <82100721+keshavg1407@users.noreply.github.com>
2021-04-15 16:33:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,136 @@
+//Game States
+var PLAY=1;
+var END=0;
+var gameState=1;
+var knifeSwooshSound;
+var knife, knifeImage;
+var knifeImage ;
+
+
+function preload(){
+  
+  knifeImage = loadImage("knife.png");
+  monsterImage = loadAnimation("alien1.png","alien2.png");
+  fruit1 = loadImage("fruit1.png");
+  fruit2 = loadImage("fruit2.png");
+  fruit3 = loadImage("fruit3.png");
+  fruit4 = loadImage("fruit4.png");
+  gameOverImage = loadImage("gameover.png")
+  knifeSwooshSound = loadSound("knifeSwoosh.mp3");
+  gameOverSound = loadSound("gameover.mp3");
+}
+
+
+
+function setup() {
+  createCanvas(600, 600);
+  
+  //creating sword
+   knife=createSprite(40,200,20,20);
+   knife.addImage(knifeImage);
+   knife.scale=0.7
+  
+  
+  //set collider for sword
+  knife.setCollider("rectangle",0,0,40,40);
+
+  score=0;
+  fruitGroup=createGroup();
+  monsterGroup = createGroup();
+  
+  //create fruit and monster Group variable here
+}
+
+function draw() {
+  background("lightblue");
+  
+  if(gameState===PLAY){
+    fruits();
+    Monster();
+    //calling fruit and monster function
+    
+    // Move knife with mouse
+    knife.y=World.mouseY;
+    knife.x=World.mouseX;
+  if(fruitGroup.isTouching(knife)){
+    fruitGroup.destroyEach();
+    knifeSwooshSound.play();
+    score = score+2;
+  }
+    else{
+      if(monsterGroup.isTouching(knife)){
+        gameState = END;
+        gameOverSound.play();
+        fruitGroup.destroyEach();
+        monsterGroup.destroyEach();
+        fruitGroup.setVelocityXEach(0);
+        monsterGroup.setVelocityXEach(0);
+        knife.addImage(gameOverImage);
+        knife.scale = 2;
+        knife.x = 300;
+        knife.y = 300;
+      }
+      
+    }
+    // Increase score if knife touching fruit
+   
+    // Go to end state if knife touching enemy
+      
+  }
+  
+  drawSprites();
+  
+  //Display score
+  textSize(25);
+  text("Score : "+ score,250,50);
+}
+function Monster(){
+  if(World.frameCount%200===0){
+    monster = createSprite(400,200,20,20);
+    monster.addAnimation("moving",monsterImage);
+    monster.y = Math.round(random(100,550));
+    monster.velocityX = -(8+(score/10))
+    monster.setLifetime = 50;
+    monsterGroup.add(monster);
+    
+    
+  }
+   
+}
+
+function fruits(){
+  if(World.frameCount%80===0){
+    position = Math.round(random(1,2))
+    fruit = createSprite(400,200,20,20);
+    if(position==1){
+      fruit.x = 600;
+      fruit.velocityX = -(7+(score/4))
+    }
+    else{
+      if(position==2){
+        fruit.x = 0;
+        fruit.velocityX = (7+(score/4))
+      }
+    }
+    fruit.scale=0.2;
+    //fruit.debug = true;
+    r = Math.round(random(1,4));
+    if(r==1){
+      fruit.addImage(fruit1)
+    } else if(r==2){
+      fruit.addImage(fruit2)
+    }
+    else if(r==3){
+      fruit.addImage(fruit3)
+    }
+    else {
+      fruit.addImage(fruit4)
+    }
+    fruit.y = Math.round(random(50,340));
+    
+    
+    fruit.setLifetime = 100;
+    
+    fruitGroup.add(fruit)
+  }
+}
